75a7e008f2aafe5cb2d52b12cbe888fd1af5c5b9
Akapass <159681952+Akapass@users.noreply.github.com>
2026-03-28 20:30:44 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,14 @@
+// Combined Monetag Service Worker Hub
+// Zones: 10798492, 10798494, 10798495
+
 self.options = {
     "domain": "3nbf4.com",
-    "zoneId": 10798500
-}
-self.lary = ""
-importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
+    "zoneId": 10798495
+};
+
+// Import all service worker scripts for your domains to ensure coverage
+importScripts('https://5gvci.com/act/files/service-worker.min.js?zoneId=10798492');
+importScripts('https://5gvci.com/act/files/service-worker.min.js?zoneId=10798494');
+importScripts('https://3nbf4.com/act/files/service-worker.min.js?zoneId=10798495');
+
+self.lary = "";
